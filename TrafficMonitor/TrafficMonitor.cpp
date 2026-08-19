5efd2159af8117301a2b6a755897aaa995887678
zhongyang219 <zhongyang219@hotmail.com>
2026-08-19 11:31:40 +0800

    app初始化时获取当前屏幕DPI，解决当主窗口还未创建时无法获取DPI的问题

--- TrafficMonitor/TrafficMonitor.cpp
+++ TrafficMonitor/TrafficMonitor.cpp
@@ -1018,6 +1018,9 @@ BOOL CTrafficMonitorApp::InitInstance()
     Gdiplus::GdiplusStartupInput gdiplusStartupInput;
     GdiplusStartup(&m_gdiplusToken, &gdiplusStartupInput, NULL);
 
+    //初始时使用屏幕DPI
+    DPIFromWindow(nullptr);
+
     //载入插件
     LoadPluginDisabledSettings();
     m_plugins.LoadPlugins();
